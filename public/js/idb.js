ca61a1b8acf87293a7ea06194f47ef9b4633d6a1
Jennifer Cole <capauldi@gmail.com>
2022-03-18 01:53:13 -0700

    indexedDB functionality added to store offline submissions for posting when reconnected

---
+++ public/js/idb.js
@@ -0,0 +1,64 @@
+let db;
+
+const windowLocation = window.location;
+if (!windowLocation.href.includes('index.html')) {
+    windowLocation.replace('./index.html');
+}
+
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+}
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+}
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+}
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+    budgetObjectStore.add(record);
+}
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const budgetObjectStore  = transaction.objectStore('new_transaction');
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(['new_transaction'], 'readwrite'); 
+                    const budgetObjectStore = transaction.objectStore('new_transaction');
+                    budgetObjectStore.clear();
+
+                    alert('Budget successfully updated!')
+                })
+                .catch(err => console.log(err));
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,14 @@
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = APP_PREFIX + 'cache-' + VERSION;
 
 const FILES_TO_CACHE = [
     './index.html',
+    './css/styles.css',
     './js/index.js',
-    './css/styles.css'
+    './js/idb.js',
+    './icons/icon-192x192.png'
 ];
 
 self.addEventListener('install', function(e) {
@@ -18,7 +21,7 @@ self.addEventListener('install', function(e) {
     self.skipWaiting();
 });
 
-self.addEventListener('activing', function(e) {
+self.addEventListener('activate', function(e) {
     e.waitUntil(
         caches.keys().then(keyList => {
             return Promise.all(
@@ -35,10 +38,35 @@ self.addEventListener('activing', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
+    if (e.request.url.includes('/api/')) {
+        e.respondWith(
+            caches.open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(e.request)
+                        .then(response => {
+                            if (response.status === 200) {
+                                cache.put(e.request.url, response.clone());
+                            }
+                            return response;
+                        })
+                        .catch(err => {
+                            return cache.match(e.request);
+                        });
+                })
+                .catch(err => console.log(err))
+        );
+        return;
+    }
     console.log('Fetch request:', e.request.url);
     e.respondWith(
-        caches.match(e.request).then(function(request) {
-            return request || fetch(e.request);
+        fetch(e.request).catch(function() {
+            return caches.match(e.request).then(function(response) {
+                if (response) {
+                    return response;
+                } else if (e.request.headers.get('accept').includes('text/html')) {
+                    return caches.match('/');
+                }
+            });
         })
     );
 });
